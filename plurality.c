de75c1ac28d88d11a18148152fd3156cdd993d61
Blasidelgado <blasidelgado@gmail.com>
2023-07-16 14:52:52 -0300

    add cs50x solution

--- plurality.c
+++ plurality.c
@@ -22,6 +22,7 @@ int candidate_count;
 // Function prototypes
 bool vote(string name);
 void print_winner(void);
+void swap(candidate *x, candidate *y);
 
 int main(int argc, string argv[])
 {
@@ -66,13 +67,74 @@ int main(int argc, string argv[])
 // Update vote totals given a new vote
 bool vote(string name)
 {
-    // TODO
+    // Iterate candidates array to find if any existing candidate was voted
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (strcmp(name, candidates[i].name) == 0)
+        {
+            candidates[i].votes++; // Update candidate votes
+            return true;
+        }
+    }
     return false;
 }
 
 // Print the winner (or winners) of the election
 void print_winner(void)
 {
-    // TODO
+    // Array to store winners
+    candidate winners[MAX];
+
+    // Sort the array using selection sort
+    for (int j = 0; j < candidate_count - 1; j++)
+    {
+        int max = candidates[j].votes;
+        for (int k = j + 1; k < candidate_count; k++)
+        {
+            if (max < candidates[k].votes)
+            {
+                max = candidates[k].votes;
+                swap(&candidates[j], &candidates[k]);
+            }
+        }
+    }
+
+    // Check if there is a legit winner
+    if (!candidates[0].votes)
+    {
+        printf("No winner\n");
+        return;
+    }
+    else
+    {
+        winners[0] = candidates[0];
+    }
+
+    // Linear Search to create a winners array
+    for (int l = 1; l < candidate_count; l++)
+    {
+        if (winners[0].votes == candidates[l].votes)
+        {
+            winners[l] = candidates[l];
+        }
+    }
+
+    // Print all winners
+    int m = 0;
+    do
+    {
+        printf("%s\n", winners[m].name);
+        m++;
+    }
+    while (winners[m].name);
+
+    return;
+}
+
+void swap(candidate *x, candidate *y)
+{
+    candidate temp = *x;
+    *x = *y;
+    *y = temp;
     return;
 }
